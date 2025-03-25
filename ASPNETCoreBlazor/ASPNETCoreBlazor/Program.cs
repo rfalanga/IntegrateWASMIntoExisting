623530f11c0029091d10264d20f802b4a3354e35
Rod Falanga <rfalanga@users.noreply.github.com>
2025-03-24 18:50:40 -0600

    I'm trying the ASP.NET Core app first

--- ASPNETCoreBlazor/ASPNETCoreBlazor/Program.cs
+++ ASPNETCoreBlazor/ASPNETCoreBlazor/Program.cs
@@ -12,4 +12,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// In the March/April 2025 CODE Magazine article, on p. 43, step 7 says to add the following 2 lines of code in the Program.cs file. What it doesn't
+// say is which Program.cs file do I add it to; the on in the ASP.NET Core app or the one in the Blazor app? I'm going to assume it's the one in the
+// ASP.NET Core app.
+app.UseBlazorFrameworkFiles();
+app.MapFallbackToFile("index.html");
+
 app.Run();
